b0b7ab81231697e4529cd7f9e7784aacb261c253
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2023-10-19 12:11:20 -0600

    feat(examples): Agrega ejemplo de uso de funciones

---
+++ examples/bad/7 - functions.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+
+/*@
+  requires x > INT_MIN;
+  ensures (x >= 0 ==> \result == x) && (x < 0 ==> \result == -x);
+  assigns \nothing;
+*/
+int abs(int x);
+
+/*@
+  requires -1000 <= x <= 1000;
+  requires -1000 <= y <= 1000;
+  ensures \result >= 0;
+  assigns \nothing;
+ */
+int sumabs(int x, int y)
+{
+  int a = abs(x);
+  int b = abs(y);
+  return a + b;
+}
+
+//@assigns \nothing;
+void foo(int a)
+{
+  int b = abs(42);
+  int c = abs(-50);
+  int d = abs(a); // !!!!!
+}
